d5e87dda1e63d6e3eed432555eb72f8a4e28cf54
katokaede <kaede.knsn1925@gmail.com>
2024-12-14 11:40:30 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,14 @@
+
+
+//課題
+//$(window).on(イベント, () => {});
+
+$(window).on('load',function(){
+    console.log('loadイベントが発生しました');
+   });
+   
+$(window).on('scroll',function(){
+    console.log('scrollイベントが発生しました');
+   });
+   
+  
